7a837bf10c6bc0a99a9f6ce2d8f079ce3836323f
Lucas Mello22 <lucas.s.mello@outlook.com>
2023-10-09 01:37:03 -0300

    add routes to pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Home from './router\'s/Home.jsx';
+import Contact from './router\'s/Contact.jsx';
 
+// configurando Router
+import {
+  createBrowserRouter, RouterProvider
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "contact",
+    element: <Contact />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
