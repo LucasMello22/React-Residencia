f5fd7a916a75b7f45d872d91f85831009416d4fa
Gildo Norberto <gildoandriano165@gmail.com>
2023-10-11 16:23:15 -0300

    Update App.jsx and App2.jsx

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import Header2 from './components/Layouts/Header2';
-import Footer from './components/Layouts/Footer';
+import Header2 from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 import { Outlet } from 'react-router-dom';
