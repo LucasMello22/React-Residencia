a7146c3be5e99fe9dcc9d3079cdbae9ec6d418e4
Lucas Mello22 <lucas.s.mello@outlook.com>
2023-10-17 14:09:12 -0300

    adjusting routes and allocating NavLink component

---
+++ src/routers/mentor/MentorCalendario.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const MenCalendario = () => {
+  return (
+    <div>
+      <center>
+      <h1>Pagina Calendario </h1>
+      
+      </center>
+    </div>
+  )
+}
+export default MenCalendario
